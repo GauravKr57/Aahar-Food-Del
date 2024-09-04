68aad5b372b64630518964eb6deedc6bf58e2998
Gaurav Kumar <118609334+GauravKr57@users.noreply.github.com>
2024-09-04 21:53:42 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "http://localhost:4000"
+  const url = "https://aahar-food-del-backend.onrender.com"
 
   return (
     <div>
